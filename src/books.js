ccb2dbbdf649021948307f90f29ed2c36cbad67b
wahyu12v <mkzcmd@gmail.com>
2024-05-27 16:28:49 +0700

    first commit

---
+++ src/books.js
@@ -0,0 +1,5 @@
+const books = [
+  { id: 1 }
+]
+
+module.exports = books
---
+++ src/server.js
@@ -0,0 +1,40 @@
+/* eslint-disable no-unused-vars */
+// const os = require("os");
+// os.hostname = () => "localhost";
+// const Hapi = require("@hapi/hapi");
+// const routes = require("./routes");
+
+// const init = async () => {
+//   const server = Hapi.server({
+//     port: 9000,
+//     host: "localhost",
+//     routes: {
+//       cors: {
+//         origin: ["*"],
+//       },
+//     },
+//   });
+
+//   server.route(routes);
+
+//   await server.start();
+//   console.log(`Server berjalan pada ${server.info.uri}`);
+
+// init();
+
+const Hapi = require('@hapi/hapi')
+const routes = require('./routes')
+
+const init = async () => {
+  const server = Hapi.server({
+    port: 5000,
+    host: 'localhost'
+  })
+
+  server.route(routes)
+
+  await server.start()
+  console.log(`Server berjalan pada ${server.info.uri}`)
+}
+
+init()
